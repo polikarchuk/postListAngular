dbeb0d52b9e0cb8c5757c55195d313376ad7d0f6
polikarchuk <polikarchuk.o@gmail.com>
2019-02-27 14:55:14 +0200

    first commit

---
+++ src/app/post.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from "@angular/common/http";
+import { Inject, Injectable } from "@angular/core";
+import { map } from "rxjs/internal/operators";
+
+@Injectable()
+export class PostService {
+    constructor(@Inject(HttpClient) private http: HttpClient) {}
+    getPosts() {
+        return this.http.get("https://jsonplaceholder.typicode.com/posts").pipe(
+            map((posts: any[]) => {
+                return posts.map(post => {
+                    return {
+                        title: post.title
+                    };
+                });
+            })
+        );
+    }
+}
